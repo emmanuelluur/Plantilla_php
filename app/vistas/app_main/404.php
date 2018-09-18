35958406053741c3e07a54048801d16b8dd66120
Emmanuel Lucio Urbina <emmanuelluur@gmail.com>
2018-09-17 22:57:39 -0500

    + 404

--- app/vistas/app_main/404.php
+++ app/vistas/app_main/404.php
@@ -2,25 +2,64 @@
 <html>
 <title>404</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="../app/vistas/libs/core.css">
+<style media="screen">
+html,body{
+  height:100%
+}
+body {
+  background:#0000aa;
+  background: linear-gradient(135deg, rgba(38,138,214,1) 0%,rgba(31,62,155,1) 100%);
+  color:#ffffff;
+  font-family: 'Share Tech Mono', monospace;
+  font-size:20px;
+  text-align:center;
+  margin:10vw;
+  overflow: hidden;
+  text-shadow:2px 2px 0 rgba(0,0,0,.16)
+}
+
+.blink {
+  animation:pulse 1.2s .1s infinite;
+  transition:all 0.1s
+}
+
+@keyframes pulse{
+  0%{ opacity:0.1 }
+}
+
+h2 {
+  background:#fff;
+  color:#246fc1;
+  padding:2px 8px;
+  width:fit-content;
+  margin:20px auto;
+  box-shadow:2px 2px 0 rgba(0,0,0,.16)
+}
+
+p {
+  margin:30px 100px;
+  text-align:left;
+}
+
+a,a:hover {
+  color:inherit;
+  text-decoration:none
+}
+
+.menu {
+  margin-top:50px;
+}
+
+</style>
 <body>
-
-<div class="w3-container">
-  <br>
-  <div class="w3-card-4" style="width:50%;">
-    <header class="w3-container w3-blue">
-      <h1>No se encontro</h1>
-    </header>
-
-    <div class="w3-container">
-      <p>404</p>
-    </div>
-
-    <footer class="w3-container w3-blue">
-
-    </footer>
+  <h2>ERROR 404</h2>
+  <b>:( Recurso: <?php echo $_GET['page']; ?> no encontrado</b>
+  <p>* ...</p>
+  <p>* Intenta otro recurso.<b class=blink>|</b></p>
+  <div class="menu">
+    <a href="javascript:history.back()">< Regresar</a> <b class=blink>|</b>
   </div>
-</div>
+
 
 </body>
 </html>
